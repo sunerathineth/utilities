db9fd11e1fa981610787e4353719c98defd14776
Sunera Amaraguru <suneraguest1@gmail.com>
2025-02-17 17:59:54 +0530

    Create XeroModel.js

---
+++ XeroModel.js
@@ -0,0 +1,46 @@
+export async function askAI(prompt, data) {
+    import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "https://cdn.skypack.dev/@google/generative-ai";
+
+    let model_apiKey = "AIzaSyAiSUQbyel_gOTBMGwRMMtpueML79oJRoo";
+    const genAI = new GoogleGenerativeAI(model_apiKey);
+
+    const safetySettings = [
+        { category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE },
+        { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE },
+    ];
+
+    const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-flash",
+        safetySettings,
+    });
+    
+    const generationConfig = {
+        temperature: 1,
+        topP: 0.95,
+        topK: 40,
+        maxOutputTokens: 8192,
+        responseMimeType: "text/plain",
+    };
+
+    const today = new Date();
+    const time_a = today.toLocaleTimeString();
+    const date_a = today.toLocaleDateString();
+
+    const chatSession = model.startChat({
+        generationConfig,
+        history: [],
+    });
+
+    const mainModelResult = await chatSession.sendMessage(`
+        @ Your details,
+        - Name: Meta Assist,
+        - Purpose: To assist with tasks and provide information.
+        - Today, date: ${date_a}, Time: ${time_a}
+        - Response format: Bullet points, Event and Result
+        - ${prompt}
+        - Data from user: ${data}
+    `);
+
+    const response = await mainModelResult.response;
+    return (await response.text()).replace(/[*_~`]/g, "").trim();
+}
